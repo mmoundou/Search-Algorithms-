dc06420daf0e4e8c110e7d0fb37f6f099e1c4a54
Matthieu Eric Moundou <38407717+mmoundou@users.noreply.github.com>
2022-05-29 12:20:14 -0400

    Created a JavaDoc for TreeDemo class.

--- src/TreeDemo.java
+++ src/TreeDemo.java
@@ -1,19 +1,72 @@
 
+/**
+ * TreeDemo models a Tree API that serves as storage structure for a state's license plates.
+ * The API allows for data structures of any size, and stores the plates
+ * in ascending order (where each plate number is greater than the one that
+ * precedes it and smaller than the one that follows it).
+ *
+ * @author Matthieu Moundou
+ *
+ */
+
 public class TreeDemo {
 
     private NodeDemo head;
 
+    // Default constructor, implemented for test purposes
     public TreeDemo() {
         head = new NodeDemo();
     }
+
+    public TreeDemo(int id) {
+        head = new NodeDemo(id);
+    }
+
+    public TreeDemo(int[] idArray) {
+
+        int[] arr = new int[idArray.length - 1];
+
+        for(int index = 1; index < idArray.length; index++)
+            arr[index - 1] = idArray[index];
+
+        head = new NodeDemo(idArray[0]);
+        head.addChildArray(arr);
+
+    }
+
+    /**
+     * addId takes in an integer parameter representing a license plate number,
+     * and loads it into the Tree's data structure.
+     *
+     * @param id - an integer value representing a license plate.
+     *
+     * */
 
     public void addId(int id) {
         head.addChild(id);
     }
 
+    /**
+     * addIdArray takes in a list of integer values (in the form of an array)
+     * representing a license plate numbers, and loads them into the Tree's data structure.
+     *
+     * @param idArray - an integer array representing a set of license plates.
+     *
+     * */
+
     public void addIdArray(int[] idArray) {
         head.addChildArray(idArray);
+
     }
+
+    /**
+     * locate takes in an integer value, searches the data structure for that value,
+     * and returns a boolean value indicating whether the value was found or not.
+     *
+     * @param id - an integer value representing the search value.
+     * @return - boolean value indicating that the search was successful or unsuccessful
+     *
+     */
 
     public boolean locate(int id) {
 
